2927183d3098a528d67eb1ca2f8aa6fbb2117861
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-02-06 11:29:53 -0300

    Factory Test - WIP

---
+++ test/testMagnethFactory.js
@@ -0,0 +1,60 @@
+const Wallet = require('ethereumjs-wallet');
+const ethUtils = require('ethereumjs-util')
+const { bytecode:magnethBytecode } = require('./../build/contracts/Magneth.json')
+
+const {
+  deployFactory,
+  deployMagneth,
+  buildCreate2Address,
+  numberToUint256,
+  encodeParam,
+  isContract
+} = require('./utils')
+
+const privateKeys = [
+    ethUtils.toBuffer('0xced26e4f0ad256777efa4b205ac3003eca7e1befb9f657be58600b7115a6cdf1'),
+    ethUtils.toBuffer('0x3132ce18b38230af1f8d751f5658c97e59d33a9e884676fddfc9cc4434cd36fb'),
+    ethUtils.toBuffer('0x087df46b73931fd31751e80a203bb6be011f3ab2cf1930b2a92db901f0fdffc6'),
+    ethUtils.toBuffer('0xeb558208fc7e52bc018d11414e6e624d0ab44a7cb63dfad9d75f913b45268746'),
+    ethUtils.toBuffer('0xde43de7119a20ee767b39b926058096f95812058ed1c078f35269b5c788a33cf'),
+];
+
+const wallets = [
+    Wallet.fromPrivateKey(privateKeys[0]),
+    Wallet.fromPrivateKey(privateKeys[1]),
+    Wallet.fromPrivateKey(privateKeys[2]),
+    Wallet.fromPrivateKey(privateKeys[3]),
+    Wallet.fromPrivateKey(privateKeys[4])
+]
+
+contract('Magneth', () => {
+    
+  it('Should create magneth wallet', async () => {
+    const fromAddress = wallets[0].getAddress()
+    console.log(fromAddress)
+    const factoryAddress = await deployFactory(fromAddress)
+    console.log(factoryAddress)
+    const salt = 1
+  
+    console.log(factoryAddress)
+  
+    const bytecode = `${magnethBytecode}${encodeParam('address', fromAddress).slice(2)}`
+  
+    const computedAddr = buildCreate2Address(
+        factoryAddress,
+        numberToUint256(salt),
+        bytecode
+    )
+  
+    console.log(computedAddr)
+    console.log(await isContract(computedAddr))
+  
+    const result = await deployMagneth(fromAddress, factoryAddress, salt)
+  
+    console.log(result.txHash)
+    console.log(result.address)
+  
+    console.log(await isContract(computedAddr))
+  })
+
+})
--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,7 @@
 const ethUtils = require('ethereumjs-util')
+const { abi:factoryAbi, bytecode:factoryBytecode } = require('./../build/contracts/MagnethFactory.json')
+const { bytecode:magnethBytecode } = require('./../build/contracts/Magneth.json')
+
 
 function getParamFromTxEvent(transaction, paramName, contractFactory, eventName) {
     assert.isObject(transaction)
@@ -65,6 +68,71 @@ async function assertThrowsAsynchronously(test, error) {
     throw new Error("Missing rejection" + (error ? " with "+error.name : ""));
 }
 
+async function deployFactory(fromAddress) {
+  const factory = new web3.eth.Contract(factoryAbi)
+
+  console.log(factoryBytecode)
+  const {_address: factoryAddress} = await factory.deploy({
+      data: factoryBytecode
+  }).send({
+    from: fromAddress
+  })
+
+  console.log(factoryAddress)
+  return factoryAddress
+}
+
+async function deployMagneth(fromAddress, factoryAddress, salt) {
+  const factory = new web3.eth.Contract(factoryAbi, factoryAddress)
+  const nonce = await web3.eth.getTransactionCount(fromAddress)
+  const bytecode = `${magnethBytecode}${encodeParam('address', fromAddress).slice(2)}`
+  const result = await factory.methods.deploy(bytecode, salt).send({
+    from: fromAddress,
+    gas: 4500000,
+    gasPrice: 10000000000,
+    nonce
+  })
+
+  const computedAddr = buildCreate2Address(
+    factoryAddress,
+    numberToUint256(salt),
+    bytecode
+  )
+
+  const addr = result.events.Deployed.returnValues.addr.toLowerCase()
+  assert.equal(addr, computedAddr)
+
+  return {
+    txHash: result.transactionHash,
+    address: addr,
+    receipt: result
+  }
+}
+
+function buildCreate2Address(creatorAddress, saltHex, byteCode) {
+  return `0x${web3.utils.sha3(`0x${[
+    'ff',
+    creatorAddress,
+    saltHex,
+    web3.utils.sha3(byteCode)
+  ].map(x => x.replace(/0x/, ''))
+  .join('')}`).slice(-40)}`.toLowerCase()
+}
+
+function numberToUint256(value) {
+  const hex = value.toString(16)
+  return `0x${'0'.repeat(64-hex.length)}${hex}`
+}
+
+function encodeParam(dataType, data) {
+  return web3.eth.abi.encodeParameter(dataType, data)
+}
+
+async function isContract(address) {
+  const code = await web3.eth.getCode(address)
+  return code.slice(2).length > 0
+}
+
 Object.assign(exports, {
     getParamFromTxEvent,
     increaseTimestamp,
@@ -72,4 +140,10 @@ Object.assign(exports, {
     assertThrowsAsynchronously,
     formatSignature,
     concatSignature,
+    deployFactory,
+    deployMagneth,
+    buildCreate2Address,
+    numberToUint256,
+    encodeParam,
+    isContract
 })
