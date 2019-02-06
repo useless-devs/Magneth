e6a5dec190c2c350c5764c025adddcafc4ba5975
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-02-06 11:29:08 -0300

    [CONFIG] compiler.

--- truffle.js
+++ truffle.js
@@ -7,5 +7,19 @@ module.exports = {
       gas: 4000000,
       gasPrice: 10000000000, // 10 gwei
     }
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+        version: '0.5.2',    // Fetch exact version from solc-bin (default: truffle's version)
+        docker: false,       // Use "0.5.1" you've installed locally with docker (default: false)
+        settings: {          // See the solidity docs for advice about optimization and evmVersion
+            optimizer: {
+                enabled: false,
+                runs: 200,
+            },
+            evmVersion: 'constantinople',
+        },
+    },
   }
 };
